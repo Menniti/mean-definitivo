84cc432567a234bb07881fe6cc3f953146d9e13b
menniti <luiz.otavio.aquino.menniti@gmail.com>
2017-04-30 15:50:55 -0300

    configurando index routes

--- app.js/app_server/routes/index.js
+++ app.js/app_server/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main');
+var ctrlLocations = require('../controllers/locations');
+var crtlOthers = require('../controllers/others');
 
+/* GET locations pages. */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
 
-/* GET home page. */
-router.get('/', ctrlMain.index);
+/* GET Other pages */
+router.get('/about', crtlOthers.about);
 
 module.exports = router;
