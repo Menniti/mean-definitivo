098d288b4987513d548d3c77008180ce0401e27a
menniti <luiz.otavio.aquino.menniti@gmail.com>
2017-05-08 20:20:17 -0300

    atualizando controlador para fornecimento dos dados para view

--- app.js/app_server/controllers/locations.js
+++ app.js/app_server/controllers/locations.js
@@ -3,7 +3,14 @@
 /* Get homeList */
 
 module.exports.homelist = function(req, res){
-	res.render('locations-list', {title: 'Home'});
+	res.render('locations-list', {
+		title: 'Loc8r - find a place to work with wifi',
+		pageHeader: {
+			title: 'Loc8r',
+			strapline: 'Find places to work with wifi near you!'
+		}
+
+	});
 };
 
 /* Get locationInfo */
