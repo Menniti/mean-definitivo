dccc694f1710cbf1b202c5a80b49b449fee5dff2
menniti <luiz.otavio.aquino.menniti@gmail.com>
2017-05-09 20:51:26 -0300

    add eventos no db.js /connect/disconnect/Err

--- app.js/app_server/models/db.js
+++ app.js/app_server/models/db.js
@@ -1,1 +1,16 @@
 var mongoose = require('mongoose');
+var dbURI = 'mongodb://localhost/loc8r';
+
+mongoose.connect('dbURL');
+
+mongoose.connect.on('connect', function(){
+	console.log('Mongoose connect to'+ dbURI);
+});
+
+mongoose.connect.on('error', function(err){
+	console.log('Mongoose connection error' + err);
+});
+
+mongoose.connect.on('disconnected', function(){
+	console.log('Mongoose disconnect');
+});
