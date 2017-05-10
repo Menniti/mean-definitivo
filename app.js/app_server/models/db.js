b171b478afc54ac75ecef9ccc04771c7f14778eb
menniti <luiz.otavio.aquino.menniti@gmail.com>
2017-05-09 21:37:38 -0300

    add gradefulShotDown listeners in db.js

--- app.js/app_server/models/db.js
+++ app.js/app_server/models/db.js
@@ -15,6 +15,8 @@ mongoose.connection.on('disconnected', function(){
 	console.log('Mongoose disconnect');
 });
 
+// Eventos de encerramento de processos
+
 var gracefulShutdown = function(msg, callback) {
 	mongoose.connection.close(function (){
 		console.log('Mongoose disconnect through' + msg);
@@ -22,3 +24,28 @@ var gracefulShutdown = function(msg, callback) {
 	});
 };
 
+// rastreadores quando a aplicação reinicia
+
+// rastreia o sinal SIGUSR2 que o nodemon usa
+process.once('SIGUSR2', function(){
+	gracefulShutdown('nodemon restart', function(){
+		// envia o sinal para matar o processo do SIGUSR2
+		process.kill(process.pid, 'SIGUSR2')
+	});
+});
+
+// Rastreia o sinal SIGINT emitido quando a aplicação é encerrada
+process.on('SIGINT', function(){
+	gracefulShutdown('app terminator', function(){
+		// Envia mensagem função gracefulShutdown em callback para encerrar o processo NODE.js
+		process.exit(0);
+	});
+});
+
+// Rastreia sinal SIGTERM emitido pelo Heroku quando encerra um processo
+process.on('SIGTERM', function(){
+	gracefulShutdown('Heroku app shutdown', function(){
+		// Envia mensagem função gracefulShutdown em callback para encerrar o processo NODE.js
+		process.exit(0);
+	});
+});
