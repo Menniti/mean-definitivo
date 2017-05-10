2d9102236ae22bcc67c7f30952f96d4c84421683
menniti <luiz.otavio.aquino.menniti@gmail.com>
2017-05-09 21:10:21 -0300

    add mongoose connection to db.js and connect to mongodb

--- app.js/app_server/models/db.js
+++ app.js/app_server/models/db.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
-var dbURI = 'mongodb://localhost/loc8r';
+var dbURI = 'mongodb://127.0.0.1:27017/Loc8r';
 
-mongoose.connect('dbURL');
+mongoose.connect(dbURI);
 
 mongoose.connection.on('connect', function(){
 	console.log('Mongoose connect to'+ dbURI);
