0cb1501573f6865de6439aae061ef397e55a4051
menniti <luiz.otavio.aquino.menniti@gmail.com>
2017-05-15 20:25:04 -0300

    modify model

--- app.js/app_server/models/locations.js
+++ app.js/app_server/models/locations.js
@@ -1,3 +1,22 @@
 
 var mongoose = require('mongoose');
 
+var locationScheema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true
+	},
+	address: String,
+	rating: {
+		type: Number,
+		"default": 0,
+		max: 5,
+		min: 0 
+	},
+	// declarando array
+	facilities: [String],
+	coords: {
+		type: [Number],
+		index: '2dsphere'
+	}
+});
