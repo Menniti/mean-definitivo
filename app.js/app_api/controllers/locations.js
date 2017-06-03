ae06930a2e1d7efbae20a1f970c256e23f3cf030
menniti <luiz.otavio.aquino.menniti@gmail.com>
2017-06-02 22:02:17 -0300

    Adicionando o method post /locations

--- app.js/app_api/controllers/locations.js
+++ app.js/app_api/controllers/locations.js
@@ -37,7 +37,33 @@ var theEarth = (function(){
 // Nova funcao sendo chamada em cada função do controlador
 // cria um novo location
 module.exports.locationsCreate = function(req, res){
-	sendJsonResponse(res, 200, {'status': 'success LocationCreate'});
+	Loc.create({
+		// aplica o método create no modelo
+		name: req.body.name,
+		address: req.body.address,
+		// cria um array com itens de facilities, a partir de uma lista separada por virgulas
+		facilities: req.body.facilities.split(","),	
+		/// converte coordenadas de string para numeros
+		coords: [parseFloat(req.body.lng), parseFloat(req.body.lat)],
+		openingTimes : [{
+			days: req.body.days1,
+			opening: req.body.opening1,
+			closing: req.body.closing1,
+			closed: req.body.closed1,
+		},{
+			days: req.body.days2,
+			opening: req.body.opening2,
+			closing: req.body.closing2,
+			closed: req.body.closed2,
+		}]
+	}, function(err, location){
+		// funcao de callback
+		if(err){
+			sendJsonResponse(res, 400, err);
+		} else {
+			sendJsonResponse(res, 201, location);
+		}
+	});
 };
 
 // Le um location
